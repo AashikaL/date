46cbe23aeebb2de8737e80a12fe67fbe862c3aad
AashikaL <aashikaaashika1799@gmail.com>
2023-08-12 12:33:48 +0530

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+modes = ['date', 'date-time', 'month', 'month-year', 'time', 'time-date', 'year'];
+selectedMode = 'date';
+showPicker = false;
+dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
+formattedString = '';
+@ViewChild(IonDatetime) datetime!: IonDatetime
 
-  constructor() {}
+  constructor() {
+    this.setToday();
+  }
+
+  setToday(){
+    this.formattedString = format(parseISO(format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z'), 'HH:mm, MMM d, yyyy');
+  }
+  dateChanged(value: any){
+    this.dateValue = value;
+    this.formattedString = format(parseISO(value), 'HH:mm , MMM d, yyyy');
+    this.showPicker = false;
+  }
+  close(){
+    this.datetime.cancel(true);
+    }
+    select(){
+    this.datetime.confirm(true);
+    }
 
 }
